89117cbc2f1aa06872894041855c1e2a18305010
Berdigylyc <146063996+Berdigylyc@users.noreply.github.com>
2023-10-25 13:37:45 +0300

    Add files via upload

--- 8-1.py
+++ 8-1.py
@@ -6,12 +6,12 @@ import matplotlib.ticker as ticker
 with open ('settings.txt') as file:
     settings=[float(i) for i in file.read().split('\n')]
 
-data=numpy.loadtxt('data.txt',dtype=int)*settings[1]
+data = numpy.loadtxt('data.txt',dtype=int)*settings[0]
 
-data_time=numpy.array([i*settings[0] for i in range(data.size)])
+data_time = numpy.array([i*settings[1] for i in range(data.size)])
 
 fig, ax=pyplot.subplots(figsize=(16,10),dpi=200)
-ax.axis([data.min(), data_time.max()+1, data.min(), data.max()+0.2])
+ax.axis([data.min(), data_time.max()+0.2, data.min(), data.max()+0.2])
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.5))
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
 ax.set_title("\n".join(wrap('The process of charging and discharching the capacitor in an RC circuit',60)), loc='center')
@@ -28,15 +28,10 @@ ax.scatter(data_time[0:data.size:20],data[0:data.size:20], marker = 's', c = 'bl
 
 ax.legend(shadow = False)
 
-ax.plot(data_time, data, c='blue', linewidth=1, label = 'charging time is 4.21s')
-ax.scatter(data_time[0:data.size:20],data[0:data.size:20], marker = 's', c = 'blue', s=10)
+ax.text(0.5, 0.75, 'charging time is 4.21s', transform=ax.transAxes)
+ax.text(0.5, 0.6, 'discharging time is 5.65s', transform=ax.transAxes)
 
-ax.legend(shadow = False)
-
-ax.plot(data_time, data, c='blue', linewidth=1, label = 'discharging time is 5.65s')
-ax.scatter(data_time[0:data.size:20],data[0:data.size:20], marker = 's', c = 'blue', s=10)
-
-ax.legend(shadow = False)
+#\n charging time is 4.21s\n discharging time is 5.65s
 
 pyplot.show()
 
